bba9a48cb63d1e208a4a291ba4812d262154294e
pajecawav <pajecawav@gmail.com>
2026-04-18 23:57:25 +0300

    feat: abstract away client/server entries

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,38 +1,13 @@
 import { defineConfig } from "vite";
 import yamf from "yamf/vite";
-import { globSync } from "tinyglobby";
 
 export default defineConfig({
 	optimizeDeps: {
 		exclude: ["yamf"],
-		entries: ["./src/client/index.ts", "./src/**/*.island.*"],
 	},
-	plugins: [
-		yamf({
-			nitro: {
-				serverDir: "./src",
-			},
-		}),
-	],
+	plugins: [yamf()],
 	build: {
 		sourcemap: true,
-	},
-	environments: {
-		ssr: {
-			build: {
-				rollupOptions: {
-					input: "./src/entry-server.tsx",
-				},
-			},
-		},
-		client: {
-			build: {
-				sourcemap: true,
-				rolldownOptions: {
-					input: ["./src/client/index.ts", ...globSync("./src/**/*.island.*")],
-				},
-			},
-		},
 	},
 	resolve: {
 		tsconfigPaths: true,
--- src/vite/index.ts
+++ src/vite/index.ts
@@ -1,22 +1,47 @@
-import { nitro, type NitroPluginConfig } from "nitro/vite";
+import { nitro } from "nitro/vite";
 import type { PluginOption } from "vite";
 import { islands } from "./islands";
 import { virtualAssets } from "./virtual-assets";
 import { virtualPages } from "./virtual-pages";
 
-interface YamfOptions {
-	nitro?: NitroPluginConfig;
-}
-
-const yamf = (options: YamfOptions = {}): PluginOption[] => {
+const yamf = (): PluginOption[] => {
 	const plugins: PluginOption[] = [];
+
+	// TODO: extendable config
+	plugins.push({
+		name: "yamf:config",
+		config() {
+			return {
+				environments: {
+					ssr: {
+						build: {
+							rollupOptions: {
+								input: "./src/entry-server.tsx",
+							},
+						},
+					},
+					client: {
+						build: {
+							rolldownOptions: {
+								input: ["./src/client/index.ts"],
+							},
+						},
+					},
+				},
+			};
+		},
+	});
 
 	plugins.push(islands());
 
 	plugins.push(virtualAssets());
 	plugins.push(virtualPages());
 
-	plugins.push(nitro(options.nitro));
+	plugins.push(
+		nitro({
+			serverDir: "./src",
+		}),
+	);
 
 	return plugins;
 };
